106e1ab24075dad8086d40b399321a271a455072
Carsten Kragelund <carsten@kragelund.me>
2023-07-04 17:57:44 +0200

    build(nix): refactor to allow building without the use of flakes
    This changes the default.nix file to use fetchTarball to pull naersk
    when using `nix-build` rather than via flakes

--- default.nix
+++ default.nix
@@ -1,11 +1,15 @@
 {
-  lib,
-  system,
-  naersk,
+  pkgs ? import <nixpkgs> {},
+  lib ? pkgs.lib,
+  system ? pkgs.system,
+  naersk ? pkgs.callPackage (builtins.fetchTarball {
+    url = "https://github.com/nix-community/naersk/archive/8507af04eb40c5520bd35d9ce6f9d2342cea5ad1.tar.gz";
+    sha256 = "sha256:0x024pcj1jwnsdx2lkm12q9zclmrsd74xrvghb2a4qjjnsvywx4d";
+  }) {},
 }: let
   manifest = (lib.importTOML ./Cargo.toml).package;
 in
-  naersk.lib."${system}".buildPackage {
+  naersk.buildPackage {
     inherit (manifest) version;
     pname = manifest.name;
     root = lib.cleanSource ./.;
