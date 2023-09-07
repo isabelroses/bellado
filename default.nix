fd7c5355126b2b0daa90f84d6c458f750a108de3
isabel <isabel@isabelroses.com>
2023-09-07 12:22:39 +0100

    flake: fix building with nix-build and nix build

--- default.nix
+++ default.nix
@@ -1,29 +1,8 @@
-{
-  pkgs ? import <nixpkgs> {
-    overlays = [
-      (import (builtins.fetchTarball {
-        url = "https://github.com/oxalica/rust-overlay/archive/c3e43223dece545cfe06ddd92fd782adc73d56c3.tar.gz";
-        sha256 = "sha256-wOmpZis06pVKTR+5meGwhrW10/buf98lnA26uQLaqek=";
-      }))
-    ];
-  },
-  lib ? pkgs.lib,
-  system ? pkgs.system,
-  naersk ? (
-    let toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml; in
-    pkgs.callPackage (builtins.fetchTarball {
-      url = "https://github.com/nix-community/naersk/archive/8507af04eb40c5520bd35d9ce6f9d2342cea5ad1.tar.gz";
-      sha256 = "sha256-jXTut7ZSYqLEgm/nTk7TuVL2ExahTip605bLINklAnQ=";
-    }) {
-      cargo = toolchain;
-      rustc = toolchain;
-    }
-  ),
-}: let
-  manifest = (lib.importTOML ./Cargo.toml).package;
-in
-  naersk.buildPackage {
-    inherit (manifest) version;
-    pname = manifest.name;
-    root = lib.cleanSource ./.;
-  }
+{pkgs ? import <nixpkgs> {}}:
+pkgs.rustPlatform.buildRustPackage {
+  pname = "bellado";
+  version = "1.4.0";
+
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+}
--- flake.nix
+++ flake.nix
@@ -1,59 +1,18 @@
 {
   description = "bellado";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      flake = false;
-    };
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
+  outputs = {nixpkgs, ...}: let
     forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" "aarch64-darwin"];
-    pkgs = system: (import nixpkgs {
-      inherit system;
-      overlays = [
-        (import inputs.rust-overlay)
-      ];
-    });
-    toolchain = system: (pkgs system).rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-    naersk = system: let
-      toolchain' = toolchain system;
-    in
-      (pkgs system).callPackage inputs.naersk {
-        cargo = toolchain';
-        rustc = toolchain';
-      };
-  in rec {
-    packages = forAllSystems (system: rec {
-      bellado = (pkgs system).callPackage ./. {
-        naersk = (naersk system).lib.${system};
-      };
-      default = bellado;
+    pkgsForEach = nixpkgs.legacyPackages;
+  in {
+    packages = forAllSystems (system: {
+      default = pkgsForEach.${system}.callPackage ./default.nix {};
     });
 
-    # For `nix develop` (optional, can be skipped):
-    devShells = forAllSystems (system: rec {
-      bellado = (pkgs system).mkShell {
-        nativeBuildInputs = [(toolchain system)];
-      };
-      default = bellado;
+    devShells = forAllSystems (system: {
+      default = pkgsForEach.${system}.callPackage ./shell.nix {};
     });
-
-    overlays = rec {
-      bellado = final: prev: {
-        bellado = self.packages.${prev.system}.bellado;
-      };
-      default = bellado;
-    };
   };
 }
