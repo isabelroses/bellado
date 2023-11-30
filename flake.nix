f97329bfa5479e31bed151eb2823d31d41c47744
isabel <isabel@isabelroses.com>
2023-11-30 20:35:20 +0000

    fix(nix): get `self` from output

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {nixpkgs, self, ...}: let
     forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" "aarch64-darwin"];
     pkgsForEach = nixpkgs.legacyPackages;
   in {
