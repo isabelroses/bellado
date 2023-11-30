b325fdfc0645f0ca363e8c015ff7c1d1028ae17e
isabel <isabel@isabelroses.com>
2023-11-30 21:57:50 +0000

    bump: nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
   description = "bellado";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
-  outputs = {nixpkgs, self, ...}: let
+  outputs = {
+    nixpkgs,
+    self,
+    ...
+  }: let
     forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" "aarch64-darwin"];
     pkgsForEach = nixpkgs.legacyPackages;
   in {
