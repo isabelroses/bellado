144767b7926db7232dd63ece395fa9355c650d67
isabel <isabel@isabelroses.com>
2023-12-01 00:18:11 +0000

    feat: add more fomating options ad use lazy_static

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use clap::{CommandFactory, Parser};
 use clap_complete::generate;
-use prettytable::{format, row, Cell, Row, Table};
+use prettytable::{format, Cell, Row, Table};
 use serde_json;
 
 mod cli;
@@ -9,6 +9,7 @@ mod git;
 mod io;
 mod tasks;
 use cli::{Cli, Commands};
+mod constants;
 
 fn main() {
     let args = Cli::parse();
@@ -45,10 +46,11 @@ fn main() {
                 all,
                 complete,
                 header,
+                as_table,
                 categories,
             } => {
                 let tasks = tasks::get_all(all, complete, categories)?;
-                display_tasks(tasks, header)?;
+                display_tasks(tasks, header, as_table)?;
             }
             Commands::Json { pretty } => {
                 let store = &tasks::load(&io::get_datastore_file()?)?;
@@ -75,14 +77,15 @@ fn main() {
             Commands::Clear {} => {
                 tasks::reset()?;
             }
-            Commands::Get { task } => {
+            Commands::Get {
+                task,
+                as_table,
+                header,
+            } => {
                 let fetched_task = tasks::get(task)?;
 
                 if let Some(task) = fetched_task {
-                    let mut table = Table::new();
-                    table.set_format(format::FormatBuilder::new().padding(1, 1).build());
-                    add_task(task, &mut table)?;
-                    table.printstd();
+                    display_tasks(vec![task], header, as_table)?;
                 } else {
                     println!("No task found with ID {task}");
                 }
@@ -111,42 +114,34 @@ pub fn init() -> Result<()> {
     Ok(())
 }
 
-fn display_tasks(tasks: Vec<tasks::Task>, header: bool) -> Result<()> {
+fn display_tasks(tasks: Vec<tasks::Task>, header: bool, as_table: bool) -> Result<()> {
     let mut table = Table::new();
 
-    table.set_format(format::FormatBuilder::new().padding(1, 1).build());
+    if as_table {
+        table.set_format(*format::consts::FORMAT_BOX_CHARS);
+    } else {
+        table.set_format(format::FormatBuilder::new().padding(1, 1).build());
+    }
 
     if header {
-        table.set_titles(row![
-            "ID",
-            "Task",
-            "Categories",
-            "Created At",
-            "Completed At",
-            "Completed"
-        ]);
+        table.set_titles(constants::HEADER.clone());
     }
 
     for task in tasks {
-        add_task(task, &mut table)?;
+        let categories = task.categories.join(", ");
+
+        table.add_row(Row::new(vec![
+            Cell::new(&task.id.to_string()),
+            Cell::new(&task.text),
+            Cell::new(&categories),
+            Cell::new(&task.created_at),
+            Cell::new(&task.completed_at.unwrap_or_else(|| "".to_string())),
+            Cell::new(if task.completed { "✓" } else { "✗" }),
+        ]));
     }
 
     // Print the table to stdout
     table.printstd();
-
-    Ok(())
-}
-
-fn add_task(task: tasks::Task, table: &mut Table) -> Result<()> {
-    let categories = task.categories.join(", ");
-    table.add_row(Row::new(vec![
-        Cell::new(&task.id.to_string()),
-        Cell::new(&task.text),
-        Cell::new(&categories),
-        Cell::new(&task.created_at),
-        Cell::new(&task.completed_at.unwrap_or_else(|| "".to_string())),
-        Cell::new(if task.completed { "✓" } else { "✗" }),
-    ]));
 
     Ok(())
 }
